578bf28f9bf3a6afa2b71b317de20e2cb1fe569e
Hosam <hosamintel@gmail.com>
2023-03-20 20:52:44 +0330

    added templates and types in code

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,77 @@
 import minimist from 'minimist';
-import { red, lightBlue, lightYellow, white } from 'kolorist';
+import { red, lightYellow, white, lightRed, lightBlue } from 'kolorist';
 import path from 'node:path';
 import prompts from 'prompts';
 import fs from 'node:fs';
 
 const cwd = process.cwd();
-const args = minimist<{ template?: string; t?: string }>(
-  process.argv.slice(2),
-  {
-    string: ['_'],
-  }
-);
 
 type ColorType = (color: string | number) => string;
 
 type Option = {
+  id: number;
   name: string;
   displayName: string;
   color: ColorType;
 };
 
-const projectTypes: Option[] = [
+const packageBundlers: Option[] = [
   {
-    name: 'solid',
+    id: 1,
+    name: 'rollup',
+    displayName: 'Rollup',
+    color: lightRed,
+  },
+  {
+    id: 2,
+    name: 'esbuild',
+    displayName: 'Esbuild',
+    color: lightYellow,
+  },
+];
+
+const packageTypes: Option[] = [
+  {
+    id: 1,
+    name: 'javascript',
     displayName: 'Javascript',
     color: lightYellow,
   },
   {
-    name: 'solid-ts',
+    id: 2,
+    name: 'typescript',
     displayName: 'Typescript',
     color: lightBlue,
   },
 ];
 
-const templates = projectTypes.map((option: Option) => {
+const templates = packageBundlers.map((option: Option) => {
   return option.name;
 });
 
-const defaultProjectDir = 'solid-js-app';
+const types = packageTypes.map((option: Option) => {
+  return option.name;
+});
+
+const defaultProjectDir = 'my-package';
 
 async function run() {
-  const argProjectDir = args._[0];
-  const argTemplate = args.template;
+  const argBundler = undefined;
+  const argType = undefined;
 
-  let projectDir = argProjectDir || defaultProjectDir;
+  let projectDir = defaultProjectDir;
   const getProjectName = () =>
     projectDir === '.' ? path.basename(path.resolve()) : projectDir;
 
-  let cli: prompts.Answers<'projectName' | 'projectType'>;
+  let cli: prompts.Answers<'packageName' | 'packageBundler' | 'packageType'>;
 
   try {
     cli = await prompts(
       [
         {
-          type: argProjectDir ? null : 'text',
-          name: 'projectName',
-          message: white('Name Of Project: '),
+          type: 'text',
+          name: 'packageName',
+          message: white('Name Of Package: '),
           initial: defaultProjectDir,
           onState: (state) => {
             projectDir = state.value || defaultProjectDir;
@@ -66,17 +82,34 @@ async function run() {
               : `'${value}' Already Exists, Choose Another`,
         },
         {
-          type:
-            argTemplate && templates.includes(argTemplate) ? null : 'select',
-          name: 'projectType',
+          type: argBundler && templates.includes(argBundler) ? null : 'select',
+          name: 'packageBundler',
           message:
-            typeof argTemplate === 'string' && !templates.includes(argTemplate)
+            typeof argBundler === 'string' && !templates.includes(argBundler)
               ? white(
-                  `The Template Chosen (${argTemplate}) Is Not Valid, Please Choose From The List Below`
+                  `The Bundler Chosen (${argBundler}) Is Not Valid, Please Choose From The List Below`
+                )
+              : white('Select a Bundler'),
+          initial: 0,
+          choices: packageBundlers.map((type) => {
+            const color = type.color;
+            return {
+              title: color(type.displayName || type.name),
+              value: type,
+            };
+          }),
+        },
+        {
+          type: argType && types.includes(argType) ? null : 'select',
+          name: 'packageType',
+          message:
+            typeof argType === 'string' && !types.includes(argType)
+              ? white(
+                  `The Type Chosen (${argType}) Is Not Valid, Please Choose From The List Below`
                 )
               : white('Select a Type'),
           initial: 0,
-          choices: projectTypes.map((type) => {
+          choices: packageTypes.map((type) => {
             const color = type.color;
             return {
               title: color(type.displayName || type.name),
@@ -96,50 +129,51 @@ async function run() {
     return;
   }
 
-  const { projectType } = cli;
+  const { packageBundler, packageType } = cli;
+  console.log(packageBundler, packageType);
+  
+  // const root = path.join(cwd, projectDir);
+  // const template = packageBundler ? packageBundler.name : argBundler;
+  // const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
+  // const pkgManager = pkgInfo ? pkgInfo.name : 'npm';
 
-  const root = path.join(cwd, projectDir);
-  const template = projectType ? projectType.name : argTemplate;
-  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
-  const pkgManager = pkgInfo ? pkgInfo.name : 'npm';
+  // fs.mkdirSync(root, { recursive: true });
 
-  fs.mkdirSync(root, { recursive: true });
+  // console.log(`\nCreating New Project In ${root}`);
 
-  console.log(`\nCreating New Project In ${root}`);
+  // const localTemplateDir = path.join(cwd, 'templates', template);
 
-  const localTemplateDir = path.join(cwd, `${template}-template`);
+  // const write = (file: string, content?: string) => {
+  //   const targetPath = path.join(root, file);
+  //   if (content) {
+  //     fs.writeFileSync(targetPath, content);
+  //   } else {
+  //     copy(path.join(localTemplateDir, file), targetPath);
+  //   }
+  // };
 
-  const write = (file: string, content?: string) => {
-    const targetPath = path.join(root, file);
-    if (content) {
-      fs.writeFileSync(targetPath, content);
-    } else {
-      copy(path.join(localTemplateDir, file), targetPath);
-    }
-  };
+  // const pkg = JSON.parse(
+  //   fs.readFileSync(path.join(localTemplateDir, `package.json`), 'utf-8')
+  // );
 
-  const pkg = JSON.parse(
-    fs.readFileSync(path.join(localTemplateDir, `package.json`), 'utf-8')
-  );
+  // pkg.name == getProjectName();
 
-  pkg.name == getProjectName();
+  // write('package.json', JSON.stringify(pkg, null, 2));
 
-  write('package.json', JSON.stringify(pkg, null, 2));
+  // const files = fs.readdirSync(localTemplateDir);
+  // files
+  //   .filter((f) => f !== 'package.json')
+  //   .forEach((file) => {
+  //     write(file);
+  //   });
 
-  const files = fs.readdirSync(localTemplateDir);
-  files
-    .filter((f) => f !== 'package.json')
-    .forEach((file) => {
-      write(file);
-    });
-
-  console.log(`\nProject Created`);
-  console.log(`\nRun The Commands Below To Start, Happy Coding !!\n`);
-  if (root !== cwd) {
-    console.log(`  cd ${path.relative(cwd, root)}`);
-  }
-  console.log(`  ${pkgManager} install`);
-  console.log(`  ${pkgManager} run dev`);
+  // console.log(`\nProject Created`);
+  // console.log(`\nRun The Commands Below To Start, Happy Coding !!\n`);
+  // if (root !== cwd) {
+  //   console.log(`  cd ${path.relative(cwd, root)}`);
+  // }
+  // console.log(`  ${pkgManager} install`);
+  // console.log(`  ${pkgManager} run dev`);
 }
 
 try {
