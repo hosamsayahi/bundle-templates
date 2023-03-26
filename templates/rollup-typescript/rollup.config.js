3d0cf6afe33301287fc39083484f4b0ea034e8a8
Hosam <hosamintel@gmail.com>
2023-03-26 22:20:38 +0430

    fix: rollup-typescrip

--- templates/rollup-typescript/rollup.config.js
+++ templates/rollup-typescript/rollup.config.js
@@ -1,12 +1,17 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import dts from 'rollup-plugin-dts';
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
+const external = [
+  ...Object.keys(pkg.dependencies || []),
+  ...Object.keys(pkg.peerDependencies || []),
+];
+
+
 export default [
   {
-    input: 'src/index.ts',
+    input: './src/index.ts',
     output: [
       {
         file: './dist/esm/index.js',
@@ -19,15 +24,11 @@ export default [
         sourcemap: true,
       },
     ],
+    external,
     plugins: [
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
     ],
-  },
-  {
-    input: 'dist/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
   },
 ];
